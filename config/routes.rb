d83e93748bed1066e3553a1d6df2aa274fd0bc1a
andrey <litomon275@yandex.ru>
2023-01-13 14:17:12 +0300

    lesson 52.0.3

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root to: "questions#index"
-  put '/questions/:id/hide', to: 'questions#hide', as: 'hide_question'
 
-  resources :questions
+  resources :questions do
+    member do
+      put 'hide'
+    end
+  end
 end
