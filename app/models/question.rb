b88ab17a30526aeb5fa702d9f60f52af58d36026
andrey <litomon275@yandex.ru>
2023-01-23 14:26:37 +0300

    Color Picker

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,5 @@ class Question < ApplicationRecord
   belongs_to :user
   belongs_to :author, class_name: 'User', optional: true
 
-  private
-
   validates :body, presence: true, length: { maximum: 280 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,16 @@
 class User < ApplicationRecord
   has_secure_password
 
-  has_many :questions, dependent: :delete_all
+  has_many :questions, dependent: :destroy
 
   before_validation :downcase_userdata
 
-  private
-
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :nickname, presence: true, uniqueness: true, length: { maximum: 40 },
-            format: { with: /\A\w+\z/ }
-  validates :headcolor, format: { with: /\A#([[:xdigit:]]{3}){1,2}\z/i }
+  format: { with: /\A\w+\z/ }
+  validates :headcolor, allow_blank: true, format: { with: /\A#[0-9a-f]{6}\z/i }
+
+  private
 
   def downcase_userdata
     nickname.downcase!
