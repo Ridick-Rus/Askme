73c39baa7fe40f22307eb89f399cb8f0d17b020b
andrey <litomon275@yandex.ru>
2023-01-25 15:27:46 +0300

    codestyle in user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,19 @@
 class User < ApplicationRecord
   has_secure_password
 
+  include Gravtastic
+  gravtastic(secure: true, filetype: :png, size: 100, default: 'retro')
+
   has_many :questions, dependent: :destroy
 
   before_validation :downcase_userdata
 
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+
   validates :nickname, presence: true, uniqueness: true, length: { maximum: 40 },
             format: { with: /\A\w+\z/ }
-  validates :headcolor, format: { with: /\A#(\h{3}){1,2}\z/ }
 
-  include Gravtastic
-  gravtastic(secure: true, filetype: :png, size: 100, default: 'retro')
+  validates :headcolor, format: { with: /\A#(\h{3}){1,2}\z/ }
 
   def downcase_userdata
     nickname&.downcase!
