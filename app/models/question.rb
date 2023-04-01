55bf76c7b3d07f7a5303e353b55e9805a0a91e38
andrey <litomon275@yandex.ru>
2023-04-01 16:29:15 +0300

    more fixes

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,18 +1,18 @@
 class Question < ApplicationRecord
-  after_commit :save_new_tags, on: %i[ create update ]
+  after_save_commit :save_new_tags, on: %i[create update]
 
   belongs_to :user
   belongs_to :author, class_name: 'User', optional: true
 
   has_many :question_tags, dependent: :delete_all
-  has_many :tags, through: :question_tags, source: :hash_tag
+  has_many :hash_tags, through: :question_tags
 
   validates :body, presence: true, length: { maximum: 280 }
 
   def save_new_tags
-    self.tags =
+    self.hash_tags =
       "#{body} #{answer}".downcase.scan(HashTag::REGEX).uniq.map do |tag|
-        HashTag.find_or_create_by(text: tag)
+        HashTag.create_or_find_by(text: tag)
       end
   end
 end
--- app/models/question_tag.rb
+++ app/models/question_tag.rb
@@ -1,4 +1,4 @@
 class QuestionTag < ApplicationRecord
-  belongs_to :question, dependent: :destroy
-  belongs_to :hash_tag, dependent: :destroy
+  belongs_to :question
+  belongs_to :hash_tag
 end
